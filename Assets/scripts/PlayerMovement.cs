60a6f79a5029d6afd9c9f3d74201c5e2135046c4
Chris Muecke <44042214+gpATTX@users.noreply.github.com>
2021-02-16 15:08:30 -0600

    Adds simple game with environment tiles, dwarf, and movement

---
+++ Assets/scripts/PlayerMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float moveSpeed = 5f;
+	private Rigidbody2D rb;
+	private Vector2 movement;
+	private Animator animator;
+	private SpriteRenderer sr;
+
+	private void Awake() {
+		rb = GetComponent<Rigidbody2D>();
+		//animator = GetComponent<Animator>();
+		sr = GetComponent<SpriteRenderer>();
+	}
+
+	// Start is called before the first frame update
+	void Start()
+    {
+		
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		movement.x = Input.GetAxisRaw("Horizontal");
+		movement.y = Input.GetAxisRaw("Vertical");
+
+		//animator.SetFloat("Horizontal", movement.x);
+		//animator.SetFloat("Vertical", movement.y);
+		//animator.SetFloat("Speed", movement.sqrMagnitude);
+
+		if (Input.GetAxis("Horizontal") < 0) {
+			sr.flipX = true;
+		}
+		if (Input.GetAxis("Horizontal") > 0) {
+			sr.flipX = false;
+		}
+	}
+
+	private void FixedUpdate() {
+		rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+	}
+}
