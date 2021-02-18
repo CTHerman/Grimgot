c522ab14581cd17ae033cb4e29e3e35abaf6919e
devATTX <44042214+devATTX@users.noreply.github.com>
2021-02-17 19:58:25 -0600

    Adds audio manager and music

---
+++ Assets/scripts/AudioManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+	public const string GAME_OVER = "gameOver";
+	public const string INFECTED = "infected";
+
+	public static AudioClip gameOver, infected, music;
+	static AudioSource audioSource;
+	static AudioSource musicSource;
+
+	public static AudioManager instance;
+
+	private void Awake() {
+		gameOver = null;// Resources.Load<AudioClip>("Audio/Sfx/game_over");
+		infected = null;//Resources.Load<AudioClip>("Audio/Sfx/infected");
+		music = Resources.Load<AudioClip>("Audio/Music/music");
+		audioSource = this.GetComponent<AudioSource>();
+		musicSource = gameObject.transform.Find("MusicSource").GetComponent<AudioSource>();
+	}
+
+	private void Start() {
+		musicSource.loop = true;
+		musicSource.clip = music;
+		musicSource.volume = .4f;
+		musicSource.Play();
+	}
+
+	private void Update() {
+	}
+
+	public static void Play(string name) {
+		switch (name) {
+			case GAME_OVER:
+				audioSource.PlayOneShot(gameOver);
+				break;
+			case INFECTED:
+				audioSource.PlayOneShot(infected);
+				break;
+		}
+	}
+}
---
+++ Assets/scripts/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public Transform target;
+	public Vector2 size; // x and y sizes are 1 less than real room size
+	public float speed; //0.1 0.5 (between 0 and 1)
+
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+		Vector3 pos = new Vector3(
+			Mathf.RoundToInt(target.position.x / size.x) * size.x,
+			Mathf.RoundToInt(target.position.y / size.y) * size.y,
+			transform.position.z);
+		transform.position = Vector3.Lerp(transform.position, pos, speed);
+	}
+}
