f7b6865382470575f43e886da1a4b04f9ac3bdcb
CTHerman <Christhomasherman@gmail.com>
2021-02-19 13:42:56 -0600

    Switch changes color on activate

--- Assets/Switch.cs
+++ Assets/Switch.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class Switch : DestructableObject
 {
     public GameObject objectToRemove;
+    public bool activated = false;
+    public SpriteRenderer spriteRenderer;
+    public Sprite activatedSpirte;
 
     // Start is called before the first frame update
     void Start()
@@ -21,16 +24,20 @@ public class Switch : DestructableObject
 
     public override void destroy(int dwarfId)
     {
-        if (whoCanDestroy[dwarfId])
+        if(!activated && whoCanDestroy[dwarfId])
         {
             Debug.Log("remove object");
             AudioManager.Play(14);
             Destroy(objectToRemove);
+            activated = true;
+            spriteRenderer.sprite = activatedSpirte;
+
         }
         else
         {
             AudioManager.Play(15);
         }
+
     }
 
 }
